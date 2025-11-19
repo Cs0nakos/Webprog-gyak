079f0ca30bd0fce7270554080340e5ea0db88e19
Cs0nakos <martonkatonatoth@gmail.com>
2025-11-19 13:54:54 +0100

    06jav

--- 06-Óraimunka/main.js
+++ 06-Óraimunka/main.js
@@ -1,8 +1,8 @@
 let ct= document.getElementsByTagName("div")[0];
-ct = document.querySelector("div");
+//ct = document.querySelector("div");
 //ct = document.querySelectorAll("div")[0];
-let lines = _cars.split("\n");
+let lines = _cars.split("\n").splice(1);
 for (let i in lines) {
     let car = new Car(lines[i]);
-ct.appendChild(car.createCard());
+    ct.appendChild(car.createCard());
 }
